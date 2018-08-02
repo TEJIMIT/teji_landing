078064cdee11bc365c8fa383578f505ed8e99bc2
Richard Kim <kimr@mit.edu>
2018-08-01 21:47:26 -0400

    complete landing

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # The Educational Justice Institute at MIT
 
 ### First Landing Page
-
-[teji.mit.edu](http://teji.mit.edu/)
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,72 +5,41 @@ import Link from 'gatsby-link'
 export default class IndexPage extends React.Component {
   render() {
     const { data } = this.props
-    const { edges: posts } = data.allMarkdownRemark
 
     return (
       <section className="section">
-        <div className="container">
-          <div className="content">
-            <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
+        <div className="image-tint">
+          <div className="mission">
+            <div className="mission-statement">
+              <h1 className="title is-4">
+                Providing  students with non-traditional learning and social justice opportunities while preparing incarcerated students for successful reentry through education and technology.
+              </h1>
+            </div>
           </div>
-          {posts
-            .map(({ node: post }) => (
-              <div
-                className="content"
-                style={{ border: '1px solid #eaecee', padding: '2em 4em' }}
-                key={post.id}
-              >
-                <p>
-                  <Link className="has-text-primary" to={post.fields.slug}>
-                    {post.frontmatter.title}
-                  </Link>
-                  <span> &bull; </span>
-                  <small>{post.frontmatter.date}</small>
-                </p>
-                <p>
-                  {post.excerpt}
-                  <br />
-                  <br />
-                  <Link className="button is-small" to={post.fields.slug}>
-                    Keep Reading →
-                  </Link>
-                </p>
-              </div>
-            ))}
+
+          <div className="main-content">
+            <div className="box jumbo-card">
+              <p>Our website is currently under construction.  Please come back in the near future.  Subscribe to be updated when we launch our website.</p>
+              <form id="email-form" name="contact" method="POST" netlify>
+                <div className="columns is-tablet">
+                  <div className="column is-two-third">
+                    <div className="field">
+                        <input className="input is-medium" name="email" type="email" placeholder="Enter your email address" />
+                    </div>
+                  </div>
+                  <div className="column is-one-third">
+                    <div className="field">
+                      <div className="field">
+                        <button className="button is-success is-medium is-fullwidth">Subscribe</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
       </section>
     )
   }
 }
-
-IndexPage.propTypes = {
-  data: PropTypes.shape({
-    allMarkdownRemark: PropTypes.shape({
-      edges: PropTypes.array,
-    }),
-  }),
-}
-
-export const pageQuery = graphql`
-  query IndexQuery {
-    allMarkdownRemark(
-      sort: { order: DESC, fields: [frontmatter___date] },
-      filter: { frontmatter: { templateKey: { eq: "blog-post" } }}
-    ) {
-      edges {
-        node {
-          excerpt(pruneLength: 400)
-          id
-          fields {
-            slug
-          }
-          frontmatter {
-            title
-            templateKey
-            date(formatString: "MMMM DD, YYYY")
-          }
-        }
-      }
-    }
-  }
-`
