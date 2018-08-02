2c58e6e218a8823d737b86f6fc379476eddb8c1a
Richard Kim <kimr@mit.edu>
2018-08-01 23:01:10 -0400

    remove background-image tint

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,40 +6,38 @@ export default class IndexPage extends React.Component {
   render() {
     return (
       <section className="section">
-        <div className="image-tint">
-          <div className="main-content">
-            <div className="box jumbo-card">
-              <p>Our website is under construction.  Please come back in the near future.  Subscribe to our mailing list be notified when we launch our website.</p>
-              <form
-                id="email-form"
-                name="subscribe"
-                method="POST"
-                data-netlify="true"
-                data-netlify-honeypot="bot-field"
-                action="/success/"
-              >
-                <input type="hidden" name="form-name" value="subscribe" />
-                <div className="columns is-tablet">
-                  <div className="column is-three-quarters-desktop is-two-thirds-tablet">
-                    <div className="field">
-                        <input className="input is-medium" name="email" type="email" placeholder="Enter your email address" />
-                    </div>
+        <div className="main-content">
+          <div className="box jumbo-card">
+            <p>Our website is under construction.  Please come back in the near future.  Subscribe to our mailing list be notified when we launch our website.</p>
+            <form
+              id="email-form"
+              name="subscribe"
+              method="POST"
+              data-netlify="true"
+              data-netlify-honeypot="bot-field"
+              action="/success/"
+            >
+              <input type="hidden" name="form-name" value="subscribe" />
+              <div className="columns is-tablet">
+                <div className="column is-three-quarters-desktop is-two-thirds-tablet">
+                  <div className="field">
+                      <input className="input is-medium" name="email" type="email" placeholder="Enter your email address" />
                   </div>
-                  <div className="column is-one-quarter-desktop is-one-third-tablet">
+                </div>
+                <div className="column is-one-quarter-desktop is-one-third-tablet">
+                  <div className="field">
                     <div className="field">
-                      <div className="field">
-                        <button
-                          className="button is-success is-medium is-fullwidth"
-                          type="submit"
-                          >
-                            Subscribe
-                        </button>
-                      </div>
+                      <button
+                        className="button is-success is-medium is-fullwidth"
+                        type="submit"
+                        >
+                          Subscribe
+                      </button>
                     </div>
                   </div>
                 </div>
-              </form>
-            </div>
+              </div>
+            </form>
           </div>
         </div>
       </section>
